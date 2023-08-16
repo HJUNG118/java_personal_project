c5e943f1d0251fd89b330cd6f9b7bbac5a8e22ad
HJUNG118 <a38722243@gmail.com>
2023-08-16 22:36:48 +0900

    Feat: java 메서드 공부

--- new/src/App.java
+++ new/src/App.java
@@ -1,15 +1,30 @@
 public class App {
     int num1;
     static int num2;
-    public void printname(String name) {
+    App() {
+        String name = "홍길동";
+        System.out.println(name);
+    }
+    public void printname(String name) { // public형태의 메소드는 다른 클래스에서도 접근이 가능하다.
         String prtmsg = name + "hello";
         System.out.println(prtmsg);
+    }
+    public void printname(String name1, String name2) {
+        String prtmsg = name1 + name2;
+        System.out.println(prtmsg);
+    }
+    void printnames(String... name) {
+        for(String s : name) {
+            System.out.println("이름 출력");
+            System.out.printf("%s", s);
+        }
     }
     public static void main(String[] args) {
         App mc = new App();
         mc.num1 = 100;
         App.num2 = 50;
         mc.printname("홍길동");
-        System.out.println("%d, %d", mc.num1, Variables.num2);
+        mc.printname("a","b");
+        System.out.printf("%d, %d", mc.num1, App.num2);
     }
 }
