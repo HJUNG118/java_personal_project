3bd05229fb1cc4636fec93f6e0140b0ff9df2459
HJUNG118 <a38722243@gmail.com>
2023-09-01 00:32:52 +0900

    Feat: 회원 도메인 Test 완료

--- demo/src/test/java/com/example/demo/repository/MemoryMemberRepository.java
+++ demo/src/test/java/com/example/demo/repository/MemoryMemberRepository.java
@@ -1,10 +1,16 @@
 package com.example.demo.repository;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import com.example.demo.domain.Member;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class MemoryMemberRepositoryTest {
     MemberRepository repository = new MemoryMemberRepository();
+
     @Test
     public void save() {
         Member member = new Member();
@@ -12,8 +18,37 @@ class MemoryMemberRepositoryTest {
 
         repository.save(member);
         Member result = repository.findById(member.getId()).get();
+//        Assertions.assertEquals(member, result); // 앞에 요소가 기대하는 value
         System.out.println("result = " + (result == member));
     }
 
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
 
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertEquals(member1, result);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        System.out.println("result = " + (result.size() == 2));
+    }
 }
